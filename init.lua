9f49e77b11dd3e7b52e4168568b906f95b95df4f
chris-jantzen <cjantzen22@gmail.com>
2023-10-07 10:46:51 -0400

    comment environment specific config

--- init.lua
+++ init.lua
@@ -1,9 +1,10 @@
-vim.g.vscode_snippets_path = "/Users/chrisjantzen/Library/Application Support/Code/User/snippets"
+-- vim.g.vscode_snippets_path = "/Users/chrisjantzen/Library/Application Support/Code/User/snippets"
 vim.opt.relativenumber = true
+vim.opt.number = true
 
-if vim.g.neovide then
-  vim.o.guifont = "Hack Nerd Font Mono:h18"
-  vim.g.neovide_padding_top = 10
-  vim.g.neovide_input_macos_alt_is_meta = true
-  vim.g.neovide_cursor_trail_size = 0.3
-end
+-- if vim.g.neovide then
+--   vim.o.guifont = "Hack Nerd Font Mono:h18"
+--   vim.g.neovide_padding_top = 10
+--   vim.g.neovide_input_macos_alt_is_meta = true
+--   vim.g.neovide_cursor_trail_size = 0.3
+-- end
